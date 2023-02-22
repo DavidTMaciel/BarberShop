fd375c6be0eacce8e8305ffea6743c060a4085b8
DavidTMaciel <davidmacieljr122@gmail.com>
2023-02-22 19:11:48 -0300

    first commit

---
+++ projeto-barbearia/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LandingPage from "./LandingPage";
+import './App.css'
+
+function App() {
+  return (
+    <section>
+      <LandingPage />
+    </section>
+  );
+}
+
+export default App;
